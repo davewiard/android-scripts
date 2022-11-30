0902b29b5ca81119349be5fcc6cbfd02c9195f17
Dave Wiard <dave.wiard@gmail.com>
2022-11-30 15:57:58 -0800

    Added additional functionality for converting to opus file format, applying replaygain, and cleaning up

--- mp32opus.py
+++ mp32opus.py
@@ -8,19 +8,70 @@ import subprocess
 import mutagen
 import ffmpeg
 
+from audiofile import AudioFile
 from pathlib import Path
 from tags import Tags
 
+
 #
-# 1. open mp3 file
-# 2. read mp3 file tags
-# 3. run ffmpeg to convert to libopus
-# 4. add missing tags from mp3 file (lyrics, album art, etc.)
-# 5. add ReplayGain tags
-# 6. rename file, if needed
+# 1. [done] get list of mp3 files to process
+# 2. [done] open mp3 file
+# 3. [done] read mp3 file tags
+# 4. [done] run ffmpeg to convert to libopus
+# 5. add missing tags from mp3 file (album art, etc.)
+# 6. [done] add ReplayGain tags
+# 7. remove mp3 file if opus file exists
 #
 
 
+def add_replaygain_tags(filename):
+  print('Applying ReplayGain tags...')
+  process = subprocess.run(['r128gain', '-v', 'warning', filename])
+  if process.returncode != 0:
+    print('Failed to upsert ReplayGain tags')
+
+
+def convert_to_opus(input_filename, output_filename):
+  ffmpeg                                                  \
+    .input(input_filename)                                \
+    .output(output_filename, **{'acodec':  'libopus'})    \
+    .overwrite_output()                                   \
+    .run()
+
+
+def get_audio_fileset(fileset):
+  result = []
+  
+  if len(fileset) > 0:
+    return fileset
+
+  for (dir, _, files) in os.walk(os.getcwd()):
+    for f in files:
+      if is_matching_file_type(os.path.join(dir, f), [AudioFile._TYPE_MP3, AudioFile._TYPE_OGG_VORBIS]):
+        result.append(os.path.join(dir, f))
+  
+  return result
+
+
+def get_opus_filename(input_audiofile, filename):
+  if input_audiofile.type == AudioFile._TYPE_MP3:
+    return filename.replace(AudioFile._EXT_MP3, AudioFile._EXT_OGG_OPUS)
+  elif input_audiofile.type == AudioFile._TYPE_OGG_VORBIS:
+    return filename.replace(AudioFile._EXT_OGG_VORBIS, AudioFile._EXT_OGG_OPUS)
+
+
+def is_matching_file_type(filename, codecs):
+  stream = ffmpeg.probe(filename)['streams'][0]
+  codec = stream['codec_name']
+  if codec in codecs:
+    return True
+  else:
+    return False
+
+
+def remove_input_file(input_filename, output_filename):
+  if Path(output_filename).exists():
+    os.remove(input_filename)
 
 
 if __name__ == '__main__':
@@ -32,4 +83,8 @@ if __name__ == '__main__':
 
   audio_files_to_process = get_audio_fileset(sys.argv[1:])
   for audio_file_to_process in audio_files_to_process:
-    print('audio_file_to_process: %s' %  audio_file_to_convert)
+    input_audiofile = AudioFile(audio_file_to_process)
+    opus_filename = get_opus_filename(audio_file_to_process)
+    convert_to_opus(audio_file_to_process, opus_filename)
+    add_replaygain_tags(opus_filename)
+    remove_input_file(audio_file_to_process, opus_filename)
