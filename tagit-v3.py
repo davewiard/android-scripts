a8df56e059ccc22de4a5a98d189d412bab8b4d4a
Dave Wiard <dave.wiard@gmail.com>
2023-01-22 10:32:53 -0800

    Added comments with plan for tagit-v3

--- tagit-v3.py
+++ tagit-v3.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 #
-#
-#
-#
-#
-#
+# 1. get artist and title from filename and parent directory
+# 2. get data from airtable
+# 3. get data from spotify
+# 4. convert file to opus, if needed
+# 4. delete any existing metadata from opus file
+# 5. add metadata to opus file
+# 6. add replaygain tags to opus file
+# 7. rename file, if needed
 #
 
