83860d18eccc3c97ef7e798f66ed38c66cd4d088
Dave Wiard <dave.wiard@gmail.com>
2023-03-23 05:45:59 -0700

    New script for getting notification counts

---
+++ notif.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+
+# gmail:5:ncgm
+# facebook:1:ncfb
+
+packages = [
+  'com.netease.eve.en',
+  'com.hikvision.hikconnect',
+]
+
+
+def get_notifications():
+  process = subprocess.run(['/data/data/com.termux/files/usr/libexec/termux-api', 'NotificationList'])
+  return process.stdout
+
+
+if __name__ == '__main__':
+  result = get_notifications()
+  print(result)
