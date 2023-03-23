8764e919073c087751692f738ef9353201ce241e
Dave Wiard <dave.wiard@gmail.com>
2023-03-23 16:56:53 -0700

    Finished prototype

--- notif.py
+++ notif.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
 import subprocess
+import json
 
 
-packages = [
-  'com.netease.eve.en',
-  'com.hikvision.hikconnect',
+packageNames = [
+  'com.google.gmail',
+  'com.robinhood',
+  'com.thescore'
 ]
 
 
-def get_notifications():
-  process = subprocess.run(['/data/data/com.termux/files/usr/libexec/termux-api', 'NotificationList'])
-  return process.stdout
+def get_count(packageName, data):
+  count = 0
+  for item in data:
+    if item['packageName'] == packageName:
+      count = count + 1
+
+  return count
 
 
 if __name__ == '__main__':
-  result = get_notifications()
-  print(result)
+  with open('./notif.json', 'r') as f:
+    data = json.load(f)
+
+  #result = subprocess.run(['/data/data/com.termux/files/usr/libexec/termux-api', 'NotifiactionList'])
+
+  notificationCounts = []
+
+  for packageName in packageNames:
+    count = get_count(packageName, data)
+    print(packageName)
+    print(count)
+
+    notificationCounts.append({packageName: count})
+
+  print(notificationCounts)
